b9512f53a83d66b2f284ac811283fbb0a30700fa
Xrolllex <karolkaczanowski@wp.pl>
2023-09-17 22:18:11 +0200

    first try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageList = document.querySelector(".gallery");
+
+const addImageList = galleryItems.map(image => `
+    <li class="gallery__item">
+        <a class="gallery__link" href = "${image.original}">
+            <img class="gallery__image"
+            src=${image.preview} 
+            data-source=${image.original} 
+            alt=${image.description}/>
+        </a>
+    </li>
+`).join("")
+
+imageList.insertAdjacentHTML("afterbegin", addImageList) 
+
+imageList.addEventListener("click", selectImage)
+
+function selectImage(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    event.preventDefault();
+    
+    const modalImage = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`);
+    modalImage.show()
+
+    const closeImage = (event) => {
+        if (event.key === "Escape") {
+            modalImage.close();
+            document.removeEventListener("keydown", closeImage)
+        };
+    };
+
+    document.addEventListener("keydown", closeImage)
+
+};
+
+
